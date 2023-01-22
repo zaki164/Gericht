fdfe202489dcf18682b1f9cec131787548f9413d
zaki <92805900+zaki164@users.noreply.github.com>
2023-01-22 07:04:32 +0200

    edit footer svg

--- src/Sections/Footer/Footer.jsx
+++ src/Sections/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import { SubHeading } from '../../components';
 import { Row, Container, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import './Footer.scss';
 
 const Footer = () => (
@@ -12,13 +14,13 @@ const Footer = () => (
           <p className='info_text mb-0'>+1 212-344-1230</p>
           <p className='info_text'>+1 212-555-1230</p>
         </Col>
-        <Col xs={{span: 12, order: 'first'}} sm={12} md={{span: 4, order: 0}} className="p-3 mb-4 mb-md-0">
+        <Col xs={{ span: 12, order: 'first' }} sm={12} md={{ span: 4, order: 0 }} className="p-3 mb-4 mb-md-0">
           <h2>Gerícht</h2>
           <SubHeading title='"The best way to find yourself is to lose yourself in the service of others.”' />
           <div className="social">
-            <i className="fa-brands fa-facebook-f face"></i>
-            <i className="fa-brands fa-twitter twit"></i>
-            <i className="fa-brands fa-instagram ins"></i>
+            <FontAwesomeIcon icon={faFacebookF} className='face' />
+            <FontAwesomeIcon icon={faTwitter} className='twit' />
+            <FontAwesomeIcon icon={faInstagram} className='ins' />
           </div>
         </Col>
         <Col xs={12} sm={6} md={4} className="p-3 mb-4 mb-md-0">
