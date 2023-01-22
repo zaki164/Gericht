e5136613d0a4b9c5d73416a5c48226d98f400894
zaki <92805900+zaki164@users.noreply.github.com>
2023-01-22 08:03:45 +0200

    navbar Responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Container, Row } from 'react-bootstrap';
 import { images } from '../../constants';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Navbar.scss';
 import { useEffect, useRef, useState } from 'react';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
@@ -18,13 +18,13 @@ const Navbar = () => {
       <Container>
         <Row>
           <div className='header_content d-flex align-items-center justify-content-between'>
-            <NavLink exact="true" to='/'><img src={images.gericht} alt='App_logo' /></NavLink>
+            <Link exact="true" to='/'><img src={images.gericht} alt='App_logo' /></Link>
             <nav className='px-2'>
               <NavLink exact="true" to="/">Home</NavLink>
               <NavLink exact="true" to="/contact" >Contact Us</NavLink>
               <NavLink exact="true" to="/about" >About</NavLink>
             </nav> 
-            <NavLink exact="true" to="/login" className='log' >Log In</NavLink>
+            <NavLink exact="true" to="/login" className='log'>Log In</NavLink>
             <FontAwesomeIcon icon={faBars} onClick={() => settoggle(true)} />
             <div className="nav_fill" ref={ref}>
               <MdOutlineRestaurantMenu onClick={() => settoggle(false)} />
